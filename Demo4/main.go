b5f72ca9e4cb297489c4ed0540c96435989266f4
MengXLv <MengXLv@outlook.com>
2026-05-18 19:11:15 +0800

    switch分之

--- Demo4/main.go
+++ Demo4/main.go
@@ -1,1 +1,30 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Scan(&a)
+
+	if a%2 == 1 {
+		fmt.Println("Yes")
+	}
+
+	switch a {
+	case 0:
+		fmt.Println("No")
+		fallthrough
+	case 1:
+		fmt.Println("a=1")
+	default:
+		fmt.Println("a>1")
+	}
+
+	var x interface{}
+	switch i := x.(type) {
+	case nil:
+		fmt.Println("Nil")
+	default:
+		fmt.Printf("%T\n", i)
+	}
+}
